6dc83f98b9aab538ac760b5818d7decf40099d04
Agnes Kavata <kavatamusyoka24@gmail.com>
2018-05-06 15:13:15 +0300

    add a jUnit test to check if word can be splite

--- src/main/java/HangMan.java
+++ src/main/java/HangMan.java
@@ -1,18 +1,28 @@
 import java.util.Random;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.Console;
+// This checks that the function should return a string data type
 public class HangMan {
-    public String guessedWord;
-    public String myInput;
-      public String words(){
-        return "Tiger";
-      }
-      //This Functions will Show the random word
-      public String randWords(){
-        Random myRandom = new Random();
-        String[] myWords = {"Tiger","Moon","Sun","Dennis"};
-        // int myIndex = myRandom.nextInt(1);
-  
-        // guessedWord = myWords[myIndex];
-        return guessedWord;
-  
-      }
-    }
+  public String guessedWord;
+  public String myInput;
+
+  public String words() {
+    return "Tiger";
+  }
+
+  // This Functions will Show the random word
+  public String randWords() {
+    Random myRandom = new Random();
+    String[] myWords = { "Tiger", "Moon", "Sun", "Peace" };
+    int myIndex = myRandom.nextInt(1);
+
+    guessedWord = myWords[myIndex];
+    return guessedWord;
+  }
+  // /This test will detrmine if the String will be split
+  public String[] splitWords(String guessedWord){
+    String[] splitWords = guessedWord.split("");
+    return splitWords;
+  }
+}
--- src/test/java/HangManTest.java
+++ src/test/java/HangManTest.java
@@ -3,15 +3,23 @@ import static org.junit.Assert.*;
 import java.util.Random;
 
 public class HangManTest {
-    //This tests if it returns a String word
-      @Test
-      public void getStringWord(){
-        HangMan score = new HangMan();
-        assertEquals("Tiger", score.words());
-      }
-      @Test
-    public void getStringRandomword(){
-      HangMan score = new HangMan();
-      assertEquals("Tiger", score.words());
+  // This tests if it returns a String
+  @Test
+  public void getStringWord() {
+    HangMan words = new HangMan();
+    assertEquals("Tiger", words.words());
+  }
+
+  // This will Test if the random picker works
+  @Test
+  public void getRandomword() {
+    HangMan words = new HangMan();
+    assertEquals("Tiger", words.randWords());
+  }
+  @Test
+    public void checkSplitting(){
+      HangMan words = new HangMan();
+      String[] myVar = {"T","i","g","e","r"};
+      assertEquals (myVar, words.splitWords("Tiger"));
     }
 }
